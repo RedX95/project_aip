9fbf027fd95ba2899f7ad3948ca19f4357fdd6a8
RedX95 <ame598395@gmail.com>
2025-12-30 20:29:42 +0300

    4.3. СОЗДАНИЕ ШАБЛОНА

--- tv_project/routes/index.js
+++ tv_project/routes/index.js
@@ -6,30 +6,49 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
 /* Страница Samsung */
 router.get('/samsung', function(req, res, next) {
-  res.send("<h1>Страница телевизоров Samsung</h1>")
+  res.render('tv_model', {
+    title: "Телевизоры Samsung",
+    picture: "/images/samsung.jpg",
+    desc: "Ведущий производитель телевизоров с технологиями QLED, Neo QLED и микроLED"
+  });
 });
 
 /* Страница LG */
 router.get('/lg', function(req, res, next) {
-  res.send("<h1>Страница телевизоров LG</h1>")
+  res.render('tv_model', {
+    title: "Телевизоры LG",
+    picture: "/images/lg.jpg",
+    desc: "Инновационные OLED и NanoCell телевизоры с технологией webOS"
+  });
 });
 
 /* Страница Sony */
 router.get('/sony', function(req, res, next) {
-  res.send("<h1>Страница телевизоров Sony</h1>")
+  res.render('tv_model', {
+    title: "Телевизоры Sony",
+    picture: "/images/sony.jpg",
+    desc: "Телевизоры BRAVIA с процессором Cognitive Processor XR и технологией Acoustic Surface"
+  });
 });
 
 /* Страница Philips */
 router.get('/philips', function(req, res, next) {
-  res.send("<h1>Страница телевизоров Philips</h1>")
+  res.render('tv_model', {
+    title: "Телевизоры Philips",
+    picture: "/images/philips.jpg",
+    desc: "Телевизоры с технологией Ambilight для иммерсивного просмотра"
+  });
 });
 
 /* Страница TCL */
 router.get('/tcl', function(req, res, next) {
-  res.send("<h1>Страница телевизоров TCL</h1>")
+  res.render('tv_model', {
+    title: "Телевизоры TCL",
+    picture: "/images/tcl.jpg",
+    desc: "Доступные телевизоры с технологиями Mini-LED и QLED"
+  });
 });
 
 module.exports = router;
