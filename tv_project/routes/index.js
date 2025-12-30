862858a0b6a3bd38f789c57e72b027ad5bd9b33b
RedX95 <ame598395@gmail.com>
2025-12-30 20:15:53 +0300

    4.2. МАРШРУТИЗАТОР

--- tv_project/routes/index.js
+++ tv_project/routes/index.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* Страница Samsung */
+router.get('/samsung', function(req, res, next) {
+  res.send("<h1>Страница телевизоров Samsung</h1>")
+});
+
+/* Страница LG */
+router.get('/lg', function(req, res, next) {
+  res.send("<h1>Страница телевизоров LG</h1>")
+});
+
+/* Страница Sony */
+router.get('/sony', function(req, res, next) {
+  res.send("<h1>Страница телевизоров Sony</h1>")
+});
+
+/* Страница Philips */
+router.get('/philips', function(req, res, next) {
+  res.send("<h1>Страница телевизоров Philips</h1>")
+});
+
+/* Страница TCL */
+router.get('/tcl', function(req, res, next) {
+  res.send("<h1>Страница телевизоров TCL</h1>")
+});
+
 module.exports = router;
