4b0575c5da69cd427ac8227eca87666a8d0d8129
RedX95 <ame598395@gmail.com>
2025-12-31 16:01:10 +0300

    6.2. СОЗДАНИЕ МОДУЛЯ ДАННЫХ

---
+++ tv_project/data.js
@@ -0,0 +1,29 @@
+// Файл: tv_project/data.js
+var data = [{
+    title: 'Samsung QLED Q80',
+    nick: 'samsung',
+    avatar: '/images/samsung.jpg',
+    desc: 'QLED телевизор с технологией Quantum Dot, 4K разрешение, HDR10+'
+}, {
+    title: 'LG OLED C3',
+    nick: 'lg',
+    avatar: '/images/lg.jpg',
+    desc: 'OLED телевизор с процессором α9 Gen6, технологией webOS'
+}, {
+    title: 'Sony Bravia XR',
+    nick: 'sony',
+    avatar: '/images/sony.jpg',
+    desc: 'Телевизор с когнитивным процессором XR, технологией Acoustic Surface'
+}, {
+    title: 'Philips Ambilight',
+    nick: 'philips',
+    avatar: '/images/philips.jpg',
+    desc: 'Телевизор с технологией Ambilight для иммерсивного просмотра'
+}, {
+    title: 'TCL Mini LED',
+    nick: 'tcl',
+    avatar: '/images/tcl.jpg',
+    desc: 'Mini-LED телевизор с высоким уровнем яркости и контрастности'
+}];
+
+module.exports.data = data;
