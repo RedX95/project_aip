5b5b599ab30b362f7b10a996b3624fb4ff45379d
RedX95 <ame598395@gmail.com>
2026-01-02 01:05:50 +0300

    8.6. ДАННЫЕ ПРИЛОЖЕНИЯ С MONGOOSE

--- tv_project/createDB.js
+++ tv_project/createDB.js
@@ -1,30 +1,21 @@
 var mongoose = require('mongoose');
-var Tv = require('./models/tv').Tv;
-var data = require('./data.js').data;
 var async = require('async');
+var data = require('./data.js').data;
 
-// Шаг 1: Функция подключения к базе
+// Загружаем модель
+require('./models/tv').Tv;
+
+// Функция открытия соединения
 function open(callback) {
-    console.log('1. Подключение к MongoDB...');
+    console.log('1. Подключение к базе данных...');
     mongoose.connect('mongodb://localhost/tv_project');
     mongoose.connection.once('open', callback);
 }
 
-// Шаг 2: Функция удаления старых данных
-function dropDatabase(callback) {
-    console.log('2. Удаление старых данных...');
-    Tv.deleteMany({})
-        .then(function() {
-            console.log('   Старые данные удалены');
-            callback();
-        })
-        .catch(callback);
-}
-
-// Шаг 3: Функция создания индексов
+// Функция создания индексов
 function createIndexes(callback) {
-    console.log('3. Создание индексов...');
-    Tv.createIndexes()
+    console.log('2. Создание индексов...');
+    mongoose.models.Tv.createIndexes()
         .then(function() {
             console.log('   Индексы созданы');
             callback();
@@ -32,11 +23,24 @@ function createIndexes(callback) {
         .catch(callback);
 }
 
-// Шаг 4: Функция создания телевизоров из data.js
-function createTvsFromData(callback) {
-    console.log('4. Создание ' + data.length + ' телевизоров...');
+// Функция очистки базы
+function clearDatabase(callback) {
+    console.log('3. Очистка базы данных...');
+    mongoose.models.Tv.deleteMany({})
+        .then(function(result) {
+            console.log('   Удалено документов: ' + result.deletedCount);
+            callback();
+        })
+        .catch(callback);
+}
+
+// Функция создания телевизоров
+function createTvs(callback) {
+    console.log('4. Заполнение базы ' + data.length + ' телевизорами...');
     
     async.each(data, function(tvData, tvCallback) {
+        var Tv = mongoose.models.Tv;
+        
         var tv = new Tv({
             title: tvData.title,
             model: tvData.nick + '_' + Date.now() + '_' + Math.random().toString(36).substr(2, 5),
@@ -45,44 +49,36 @@ function createTvsFromData(callback) {
             price: tvData.price || 50000,
             display_technology: tvData.display_technology || 'LCD',
             features: tvData.features || ['Smart TV'],
-            energy_class: tvData.energy_class || 'A',
-            created: new Date()
+            energy_class: tvData.energy_class || 'A'
         });
         
         tv.save()
             .then(function(savedTv) {
-                console.log('   ' + savedTv.title + ' сохранен');
+                console.log('   ' + savedTv.title);
                 tvCallback();
             })
             .catch(tvCallback);
     }, callback);
 }
 
-// Шаг 5: Функция закрытия соединения
-function close(callback) {
-    console.log('5. Закрытие соединения...');
-    mongoose.disconnect()
-        .then(function() {
-            console.log('   Соединение закрыто');
-            callback();
-        })
-        .catch(callback);
-}
-
 // Основная серия задач
 async.series([
     open,
-    dropDatabase,
     createIndexes,
-    createTvsFromData,
-    close
+    clearDatabase,
+    createTvs
 ], function(err, results) {
     if(err) {
         console.log('Ошибка:', err.message);
-        process.exit(1);
     } else {
-        console.log('Все задачи выполнены успешно');
+        console.log('База данных успешно заполнена');
         console.log('Создано телевизоров: ' + data.length);
-        process.exit(0);
     }
+    
+    // Закрытие соединения
+    mongoose.disconnect()
+        .then(function() {
+            console.log('Соединение закрыто');
+            process.exit(err ? 1 : 0);
+        });
 });
