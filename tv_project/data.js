e5b444f7dba3ddc122df0b3977384724094d9ca8
RedX95 <ame598395@gmail.com>
2026-01-02 01:05:50 +0300

    8.6. ДАННЫЕ ПРИЛОЖЕНИЯ С MONGOOSE

--- tv_project/data.js
+++ tv_project/data.js
@@ -1,43 +1,49 @@
-var data = [{
-    title: 'Samsung QLED Q80',
-    nick: 'samsung_q80',
-    diagonal: 65,
-    price: 120000,
-    display_technology: 'QLED',
-    features: ['Smart TV', 'HDR10+', '4K', 'Quantum Dot'],
-    energy_class: 'A+'
-}, {
-    title: 'LG OLED C3',
-    nick: 'lg_c3',
-    diagonal: 55,
-    price: 89990,
-    display_technology: 'OLED',
-    features: ['Smart TV', 'Dolby Vision', 'webOS', 'α9 Gen6'],
-    energy_class: 'A+'
-}, {
-    title: 'Sony Bravia XR',
-    nick: 'sony_xr',
-    diagonal: 75,
-    price: 149990,
-    display_technology: 'LCD',
-    features: ['Smart TV', 'Acoustic Surface', 'Google TV', 'XR Processor'],
-    energy_class: 'A'
-}, {
-    title: 'Philips Ambilight',
-    nick: 'philips_amb',
-    diagonal: 58,
-    price: 75990,
-    display_technology: 'OLED',
-    features: ['Smart TV', 'Ambilight', 'HDR', 'P5 Processor'],
-    energy_class: 'A'
-}, {
-    title: 'TCL Mini LED',
-    nick: 'tcl_mini',
-    diagonal: 70,
-    price: 68990,
-    display_technology: 'Mini-LED',
-    features: ['Smart TV', 'Roku TV', 'HDR', 'Mini-LED Pro'],
-    energy_class: 'A+'
-}];
+var data = [
+    {
+        title: 'Samsung QLED Q80',
+        nick: 'samsung_q80',
+        diagonal: 65,
+        price: 120000,
+        display_technology: 'QLED',
+        features: ['Smart TV', 'HDR10+', '4K', 'Quantum Processor 4K'],
+        energy_class: 'A+'
+    },
+    {
+        title: 'LG OLED C3',
+        nick: 'lg_c3',
+        diagonal: 55,
+        price: 89990,
+        display_technology: 'OLED',
+        features: ['Smart TV', 'Dolby Vision', 'webOS', 'α9 Gen6 AI Processor'],
+        energy_class: 'A+'
+    },
+    {
+        title: 'Sony Bravia XR',
+        nick: 'sony_xr',
+        diagonal: 75,
+        price: 149990,
+        display_technology: 'LCD',
+        features: ['Smart TV', 'Cognitive Processor XR', 'Acoustic Surface Audio+'],
+        energy_class: 'A'
+    },
+    {
+        title: 'Philips Ambilight',
+        nick: 'philips_amb',
+        diagonal: 58,
+        price: 75990,
+        display_technology: 'OLED',
+        features: ['Smart TV', 'Ambilight', 'P5 AI Picture Engine'],
+        energy_class: 'A'
+    },
+    {
+        title: 'TCL Mini LED',
+        nick: 'tcl_mini',
+        diagonal: 70,
+        price: 68990,
+        display_technology: 'Mini-LED',
+        features: ['Smart TV', 'Roku TV', 'AiPQ 3.0 Processor'],
+        energy_class: 'A+'
+    }
+];
 
 module.exports.data = data;
