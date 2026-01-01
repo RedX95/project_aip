a4bd16b845dc2a269dd7a7b391a278a385221616
RedX95 <ame598395@gmail.com>
2026-01-01 20:36:24 +0300

    8.1. ORM

--- tv_project/createDB.js
+++ tv_project/createDB.js
@@ -1,29 +1,28 @@
 // Файл: tv_project/createDB.js
-var MongoClient = require("mongodb").MongoClient;
-var data = require("./data.js").data;
+var mongoose = require('mongoose');
 
-MongoClient.connect("mongodb://localhost:27017/tv_project", function(err, db) {
-    if(err) {
-        console.error("Ошибка подключения к MongoDB:", err.message);
-        console.log("Убедитесь, что сервер MongoDB запущен (Start-Service MongoDB)");
-        return;
-    }
-    
-    console.log("Подключение к MongoDB успешно!");
-    
-    // Очищаем базу перед заполнением
-    db.dropDatabase(function(err) {
-        if(err) throw err;
-        console.log("Старая база данных удалена");
-        
-        var collection = db.collection("tvs");
-        
-        // Вставляем все данные из модуля data.js
-        collection.insertMany(data, function(err, result) {
-            if(err) throw err;
-            console.log("Добавлено телевизоров: " + result.insertedCount);
-            console.log("Данные приложения записаны в базу данных");
-            db.close();
-        });
-    });
+mongoose.connect('mongodb://localhost/tv_project');
+
+var Tv = mongoose.model('Tv', { 
+    model: String,
+    brand: String,
+    price: Number
 });
+
+var samsungTv = new Tv({ 
+    model: 'QLED Q80',
+    brand: 'Samsung',
+    price: 120000
+});
+
+// Используем .then()/.catch() вместо callback
+samsungTv.save()
+    .then(function() {
+        console.log('✅ Телевизор сохранен в базе через Mongoose');
+    })
+    .catch(function(err) {
+        console.log('Ошибка:', err);
+    })
+    .finally(function() {
+        mongoose.disconnect();
+    });
