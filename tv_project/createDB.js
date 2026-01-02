ca7a8d68bdf829a2ce7e1f048800fef4e46b4af4
RedX95 <ame598395@gmail.com>
2026-01-02 14:38:38 +0300

    9.2. ПРОВЕРКА ИНДЕКСОВ

--- tv_project/createDB.js
+++ tv_project/createDB.js
@@ -36,28 +36,65 @@ function clearDatabase(callback) {
 
 // Функция создания телевизоров
 function createTvs(callback) {
-    console.log('4. Заполнение базы ' + data.length + ' телевизорами...');
+    console.log('4. Заполнение базы тестовыми данными...');
     
-    async.each(data, function(tvData, tvCallback) {
+    // ТЕСТОВЫЙ МАССИВ - специально с дубликатами model
+    var testData = [
+        {
+            title: 'Samsung QLED Q80',
+            model: 'TEST_DUPLICATE_MODEL',  // У всех три одинаковый model!
+            brand: 'Samsung',
+            diagonal: 65,
+            price: 120000,
+            display_technology: 'QLED',
+            features: ['Smart TV', 'HDR10+', '4K'],
+            energy_class: 'A+'
+        },
+        {
+            title: 'LG OLED C3',
+            model: 'TEST_DUPLICATE_MODEL',  // ДУБЛИКАТ!
+            brand: 'LG',
+            diagonal: 55,
+            price: 89990,
+            display_technology: 'OLED',
+            features: ['Smart TV', 'Dolby Vision'],
+            energy_class: 'A+'
+        },
+        {
+            title: 'Sony Bravia XR',
+            model: 'TEST_DUPLICATE_MODEL',  // ЕЩЕ ДУБЛИКАТ!
+            brand: 'Sony',
+            diagonal: 75,
+            price: 149990,
+            display_technology: 'LCD',
+            features: ['Smart TV'],
+            energy_class: 'A'
+        }
+    ];
+    
+    async.each(testData, function(tvData, tvCallback) {
         var Tv = mongoose.models.Tv;
         
         var tv = new Tv({
             title: tvData.title,
-            model: tvData.nick + '_' + Date.now() + '_' + Math.random().toString(36).substr(2, 5),
-            brand: tvData.title.split(' ')[0],
-            diagonal: tvData.diagonal || 55,
-            price: tvData.price || 50000,
-            display_technology: tvData.display_technology || 'LCD',
-            features: tvData.features || ['Smart TV'],
-            energy_class: tvData.energy_class || 'A'
+            model: tvData.model,  // Будет одинаковый у всех
+            brand: tvData.brand,
+            diagonal: tvData.diagonal,
+            price: tvData.price,
+            display_technology: tvData.display_technology,
+            features: tvData.features,
+            energy_class: tvData.energy_class
         });
         
         tv.save()
             .then(function(savedTv) {
-                console.log('   ' + savedTv.title);
+                console.log('   Сохранен: ' + savedTv.title + ' (model: ' + savedTv.model + ')');
                 tvCallback();
             })
-            .catch(tvCallback);
+            .catch(function(err) {
+                console.log('   ОШИБКА при сохранении ' + tvData.title + ': ' + err.message);
+                tvCallback(); // Продолжаем, несмотря на ошибку
+            });
     }, callback);
 }
 
