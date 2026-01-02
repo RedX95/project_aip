12204b0d66a6df5c90477096fdb23453e4828f81
RedX95 <ame598395@gmail.com>
2026-01-02 20:39:48 +0300

    10.3. ИЗВЛЕЧЕНИЕ ДАННЫХ ИЗ БАЗЫ

--- tv_project/routes/tv.js
+++ tv_project/routes/tv.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var Tv = require('../models/tv').Tv;
 
 // Главная страница телевизоров
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор для маршрутов, начинающихся с /tv');
 });
 
-// Страница конкретного телевизора по параметру model
+/* Страница телевизора */
 router.get('/:model', function(req, res, next) {
-  res.send(req.params.model);
+
+  Tv.findOne({ model: req.params.model }, function(err, tv) {
+    if (err) return next(err);
+
+    if (!tv) {
+      return next(new Error('Нет такого телевизора'));
+    }
+
+    res.render('tv', {
+      title: tv.title,
+      brand: tv.brand,
+      diagonal: tv.diagonal,
+      price: tv.price,
+      display_technology: tv.display_technology,
+      features: tv.features,
+      energy_class: tv.energy_class
+    });
+  });
+
 });
+
+module.exports = router;
+
 
 module.exports = router;
