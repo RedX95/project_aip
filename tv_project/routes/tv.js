7d6aaf922a6ead35600a5a30409bef8e1a85f346
RedX95 <ame598395@gmail.com>
2026-01-02 22:01:21 +0300

    10.4. ЗАПОЛНЕНИЕ МЕНЮ ИЗ БАЗЫ ДАННЫХ

--- tv_project/routes/tv.js
+++ tv_project/routes/tv.js
@@ -1,36 +1,30 @@
-var express = require('express');
-var router = express.Router();
-var Tv = require('../models/tv').Tv;
+const express = require('express');
+const router = express.Router();
+const Tv = require('../models/tv');  
 
-// Главная страница телевизоров
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор для маршрутов, начинающихся с /tv');
-});
+router.get('/:model', async (req, res, next) => {
+    try {
+        const tv = await Tv.findOne({ model: req.params.model });
 
-/* Страница телевизора */
-router.get('/:model', function(req, res, next) {
+        if (!tv) return next(new Error('Нет такого телевизора'));
 
-  Tv.findOne({ model: req.params.model }, function(err, tv) {
-    if (err) return next(err);
+        const menu = await Tv.find({}, { _id: 0, title: 1, model: 1 });
 
-    if (!tv) {
-      return next(new Error('Нет такого телевизора'));
+        res.render('tv_model', {
+            title: tv.title,
+            picture: '/images/tv.png',
+            desc: `
+Бренд: ${tv.brand}
+Диагональ: ${tv.diagonal}"
+Цена: ${tv.price} ₽
+Технология: ${tv.display_technology}
+Класс энергопотребления: ${tv.energy_class}
+`,
+            menu: menu
+        });
+    } catch (err) {
+        next(err);
     }
-
-    res.render('tv', {
-      title: tv.title,
-      brand: tv.brand,
-      diagonal: tv.diagonal,
-      price: tv.price,
-      display_technology: tv.display_technology,
-      features: tv.features,
-      energy_class: tv.energy_class
-    });
-  });
-
 });
-
-module.exports = router;
-
 
 module.exports = router;
