16428681a864d99e039aca25b3ac367b9da0f57a
RedX95 <ame598395@gmail.com>
2026-01-02 19:08:39 +0300

    10.1. ПОДКЛЮЧЕНИЕ БАЗЫ ДАННЫХ

--- tv_project/app.js
+++ tv_project/app.js
@@ -3,39 +3,55 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+
+// ПОДКЛЮЧЕНИЕ БАЗЫ ДАННЫХ (п.10.1) 
+mongoose
+  .connect('mongodb://localhost/tv_project')
+  .then(() => {
+    console.log('MongoDB подключена');
+  })
+  .catch(err => {
+    console.error('Ошибка подключения MongoDB:', err.message);
+  });
+
+//  ПОДКЛЮЧЕНИЕ МОДЕЛЕЙ 
+require('./models/tv').Tv;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+//  НАСТРОЙКА ШАБЛОНОВ 
 app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//  MIDDLEWARE 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'bower_components'))); 
 
+//  СТАТИКА 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+//  РОУТЫ 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+//  404 
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+//  ОБРАБОТКА ОШИБОК 
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
