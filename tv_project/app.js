77b8c435e633983bc7a1759f7ab7405ee4275f96
RedX95 <ame598395@gmail.com>
2026-01-03 18:10:37 +0300

    11.1. УСТАНОВКА МОДУЛЯ EXPRESS-SESSION И НАСТРОЙКА COOKIE

--- tv_project/app.js
+++ tv_project/app.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var tvRouter = require('./routes/tv');
+var session = require('express-session');
 
 // ПОДКЛЮЧЕНИЕ БАЗЫ ДАННЫХ 
 mongoose
@@ -23,6 +24,7 @@ require('./models/tv').Tv;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tvRouter = require('./routes/tv');
 
 var app = express();
 
@@ -31,20 +33,29 @@ app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//  MIDDLEWARE 
+// middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//  СТАТИКА 
+// СЕССИЯ ТУТ
+app.use(session({
+  secret: 'LgIsTv',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60 * 1000 }
+}));
+
+// статика
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
-//  РОУТЫ   
+// роуты
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tv', tvRouter);
+
 
 //  404 
 app.use(function(req, res, next) {
